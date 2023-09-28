a7e9b0fe008de597f4c56b9625b4aeabd4130ecc
DVMaja <majadreilinger@gmail.com>
2023-09-28 11:18:48 +0200

    táblák létrehozása

---
+++ viragbolt/app/Models/Virag.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Virag extends Model
+{
+    use HasFactory;
+}
